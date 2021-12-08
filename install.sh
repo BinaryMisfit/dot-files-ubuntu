1a5276dce424b4d456881d375fc1bbe9e35826a3
Willie Roberts <diagoza@me.com>
2021-12-08 21:32:15 +0200

    Cleaned up config files and added IDEA files

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+CONFIG="install.conf.yaml"
+DOT_BOT_DIR="dotbot"
+DOT_BOT_BIN="bin/dotbot"
+DOT_FILES_BASE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+pushd "$DOT_FILES_BASE" >/dev/null || exit
+git -C "$DOT_BOT_DIR" submodule sync --quiet --recursive
+git submodule update --init --recursive "${DOT_BOT_DIR}"
+"$DOT_FILES_BASE/$DOT_BOT_DIR/$DOT_BOT_BIN" -d "$DOT_FILES_BASE" -c "$CONFIG" "${@}"
+popd >/dev/null || return
