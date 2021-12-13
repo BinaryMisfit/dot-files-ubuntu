0f60bac3582610d61d25c9c5b18fcfbccd850d6b
Willie Roberts <diagoza@me.com>
2021-12-13 15:31:42 +0200

    Added conditional check before installing

--- init.sh
+++ init.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
+printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Checking sudo\n' -1
+if [ ! -f "/etc/sudoers.d/90-$USER-nopasswd" ]; then
+  echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/90-$USER-nopasswd" >/dev/null
+fi
+
 printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Updating APT sources\n' -1
-sudo add-apt-repository -nsy ppa:git-core/ppa
-sudo add-apt-repository -nsy ppa:neovim-ppa/stable
-curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+if [ ! -f "/etc/apt/sources.list.d/git-core-ubuntu-ppa-$(lsb_release -c -s).list" ]; then
+  sudo add-apt-repository -nsy ppa:git-core/ppa
+fi
+
+if [ ! -f "/etc/apt/sources.list.d/neovim-ppa-ubuntu-stable-$(lsb_release -c -s).list" ]; then
+  sudo add-apt-repository -nsy ppa:neovim-ppa/stable
+fi
+
+if [ ! -f "/etc/apt/sources.list.d/nodesource.list" ]; then
+  curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+fi
+
 printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Updating APT\n' -1
 sudo DEBIAN_FRONTEND=noninteractive apt-get -qq update
 printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Upgrading Ubuntu\n' -1
@@ -21,13 +35,27 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y -qq --no-install-recommends insta
   zsh-antigen
 
 printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Installing node packages\n' -1
-mkdir -p .npm_packages
-npm config set prefix "$HOME/.npm_packages"
-npm -g install --upgrade npm
-npm -g install --upgrade neovim
+if [ ! -d "$HOME/.npm_packages/" ]; then
+  mkdir -p .npm_packages
+fi
+
+if [ "$(npm config get prefix)" != "$HOME/.npm_packages/" ]; then
+  npm config set prefix "$HOME/.npm_packages"
+fi
+
+if npm -g --list -p | grep -q "neovim"; then
+  npm -g install neovim@latest
+fi
+
+if [ "$(npm -g outdated)" != "" ]; then
+  npm -g update
+fi
+
 printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Installing python packages\n' -1
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade pynvim
+if ! pip show pynvim >/dev/null; then
+  pip install --upgrade pynvim
+fi
+
 printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Setting Timezone\n' -1
 TIMEZONE=$(cat /etc/timezone)
 if [ "$TIMEZONE" != "Africa/Johannesburg" ]; then
