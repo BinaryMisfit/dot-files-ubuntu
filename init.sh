b52ad1886d149e47cf1e8a9eece2161e16c04bd6
Willie Roberts <diagoza@me.com>
2021-12-13 21:37:07 +0200

    Added vim replacement

--- init.sh
+++ init.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+TIMEZONE=$(cat /etc/timezone)
+if [ "$TIMEZONE" != "Africa/Johannesburg" ]; then
+  printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Setting Timezone\n' -1
+  sudo ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+  sudo dpkg-reconfigure --frontend noninteractive tzdata
+fi
+
+CUR_LOCALE=$(cat /etc/default/locale)
+if [ "$CUR_LOCALE" != "LANG=en_US.UTF-8" ]; then
+  printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Setting Locale\n' -1
+  sudo locale-gen --purge en_US.UTF-8 >/dev/null
+  sudo update-locale LANG=en_US.UTF-8
+fi
+
 if [ ! -f "/etc/sudoers.d/90-$USER-nopasswd" ]; then
   printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Updating sudo for %s\n' -1 "$USER"
   echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/90-$USER-nopasswd" >/dev/null
@@ -19,19 +33,19 @@ if [ ! -f "/etc/apt/sources.list.d/nodesource.list" ]; then
   curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 fi
 
-printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Updating APT\n' -1
+printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Updating repositories\n' -1
 sudo DEBIAN_FRONTEND=noninteractive apt-get -qq update
-printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Upgrading Ubuntu\n' -1
+printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Upgrading packages\n' -1
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -qq upgrade
 
-PACKAGES=("build-essential" "coreutils" "i2c-tools" "imagemagick" "indicator-cpufreq" "jq" "lm-sensors" "nodejs" "neofetch")
-PACKAGES+=("python3" "python3-pip" "tmux" "zsh" "zsh-antigen")
+PACKAGES=("build-essential" "coreutils" "curl" "i2c-tools" "imagemagick")
+PACKAGES+=("indicator-cpufreq" "jq" "lm-sensors" "nodejs" "neofetch")
+PACKAGES+=("python3" "python3-pip" "socat" "tmux" "zsh" "zsh-antigen")
 INSTALL=()
 for PACKAGE in "${PACKAGES[@]}"; do
   if DEBIAN_FRONTEND=noninteractive apt-cache policy "$PACKAGE" | grep -q 'Installed: (none)'; then
     INSTALL+=("$PACKAGE")
   fi
-
 done
 
 if [ ${#INSTALL[@]} -gt 0 ]; then
@@ -62,16 +76,36 @@ if ! pip show pynvim >/dev/null; then
   pip install --upgrade pynvim
 fi
 
-CUR_LOCALE=$(cat /etc/default/locale)
-if [ "$CUR_LOCALE" != "LANG=en_US.UTF-8" ]; then
-  printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Setting Locale\n' -1
-  sudo locale-gen --purge en_US.UTF-8 >/dev/null
-  sudo update-locale LANG=en_US.UTF-8
+if [ "$SHELL" != "$(which zsh)" ]; then
+  printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Settings default shell to zsh\n' -1
+  sudo chsh -z "$(which zsh)" "$USER"
 fi
 
-TIMEZONE=$(cat /etc/timezone)
-if [ "$TIMEZONE" != "Africa/Johannesburg" ]; then
-  printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Setting Timezone\n' -1
-  sudo ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-  sudo dpkg-reconfigure --frontend noninteractive tzdata
+if [ "$(which nvim)" !=  "$(update-alternatives --get-selections | grep 'editor' | awk '{ print $3 }')" ]; then
+  printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Setting default editor to neovim\n' -1
+  sudo update-alternatives --install /usr/bin/editor editor "$(which nvim)" 100 >/dev/null
 fi
+
+if [ "$(which nvim)" !=  "$(update-alternatives --get-selections | grep 'vi' | awk '{ print $3 }')" ]; then
+  printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Setting default vi to neovim\n' -1
+  sudo update-alternatives --install /usr/bin/vi vi "$(which nvim)" 100 >/dev/null
+fi
+
+OUTDATED=("vim" "vim-tiny")
+REMOVE=()
+for PACKAGE in "${OUTDATED[@]}"; do
+  if ! DEBIAN_FRONTEND=noninteractive apt-cache policy "$PACKAGE" | grep -q 'Installed: (none)'; then
+    REMOVE+=("$PACKAGE")
+  fi
+done
+
+if [ ${#REMOVE[@]} -gt 0 ]; then
+  printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Removing unneeded packages\n' -1
+  printf "%s" "${REMOVE[@]}"
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -y -qq remove --purge "${REMOVE[@]}"
+fi
+
+printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Removing outdated packages\n' -1
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y autoremove
+printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Cleaning packages\n' -1
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qq autoclean
