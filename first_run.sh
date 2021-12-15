904432f567b445c2e653564659055fb03ca992ff
Willie Roberts <diagoza@me.com>
2021-12-15 18:43:29 +0200

    Updated zprofile

--- first_run.sh
+++ first_run.sh
@@ -165,11 +165,6 @@ if sudo test ! -d "/home/binarymisfit/.dotfiles"; then
   sudo chown -R binarymisfit:binarymisfit /home/binarymisfit
 fi
 
-if sudo test -f "/home/binarymisfit/.dotfiles/install.sh" && sudo test ! -f "/home/binarymisfit/.antigenrc"; then
-  printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Installing dot files\n' -1
-  sudo -u binarymisfit -- bash -c 'cd $HOME; /home/binarymisfit/.dotfiles/install.sh'
-fi
-
 if sudo test ! -f "/etc/sudoers.d/90-binarymisfit-nopasswd"; then
   printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Updating sudo for binarymisfit\n' -1
   echo "binarymisfit ALL=(ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/90-binarymisfit-nopasswd" >/dev/null
@@ -196,11 +191,6 @@ if sudo test ! -d "/root/.dotfiles"; then
   printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Retrieving dot files\n' -1
   sudo git clone --recurse-submodules https://github.com/BinaryMisfit/dot-files-ubuntu /root/.dotfiles
   sudo chown -R root:root /root
-fi
-
-if sudo test -f "/root/.dotfiles/install.sh" && sudo test ! -f "/root/.antigenrc"; then
-  printf '[%(%a %b %e %H:%M:%S %Z %Y)T] Installing dot files\n' -1
-  sudo -u root -- bash -c 'cd $HOME; /root/.dotfiles/install.sh'
 fi
 
 if sudo test ! -f "/etc/sudoers.d/90-root-nopasswd"; then
