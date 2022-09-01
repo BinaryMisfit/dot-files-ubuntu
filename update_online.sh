8e463b1711a6e54134f9af8470ddb9b6b5adfd57
Willie Roberts <diagoza@me.com>
2022-09-01 22:33:10 +0200

    Updated github check timeout

--- update_online.sh
+++ update_online.sh
@@ -7,6 +7,6 @@ if [ -n "$TERMINAL_EMULATOR" ]; then
   exit 0
 fi
 
-if echo -e "GET http://raw.github.com HTTP/1.0\n\n" | nc github.com 80 >/dev/null 2>&1; then
+if ping -q -c1 github.com &>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.github.com/BinaryMisfit/dot-files-ubuntu/active/update_check.sh)"
 fi
